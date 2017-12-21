93846bd19114ec02c3e17c55517e546ec700c8e8
Ryan Sabo <sabora920@gmail.com>
2017-12-21 17:34:33 -0500

    still working on guess-form test file for component testing

--- src/components/__tests__/guess-form.test.js
+++ src/components/__tests__/guess-form.test.js
@@ -1,5 +1,20 @@
 import '../testConfig'
-
 import React from 'react';
+import GuessForm from '../guess-form';
+import { makeGuess } from '../../actions';
+import { shallow, mount } from 'enzyme';
 
-import GuessForm
+describe('<GuessForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessForm />);
+      });
+
+    it('should set guess to the correct number input', () =>{
+        const value = '10';
+        const spy = jest.fn();
+        const wrapper = mount(<GuessForm dispatch={spy} />);
+        wrapper.find('input[type="number"]').instance().value = value;
+        button.simulate('submit');
+        expect(spy).toHaveBeenCalledWith(makeGuess(value))
+    })
+})
